f82691c58ed863274cc9de51031255b0bfd9898b
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-01-17 19:35:27 +0530

    Lab 3

---
+++ Lab_3/Q1.c
@@ -0,0 +1,13 @@
+#include <LPC17xx.h>
+int main(){
+	LPC_PINCON->PINSEL10 = 0x00000000;
+	LPC_GPIO0->FIODIR = 0x03000000;
+	while(1){
+		if(!(LPC_GPIO2->FIOPIN & 0x00000800)){
+			LPC_GPIO0->FIOSET = 0x03000000;
+		}
+		else{
+			LPC_GPIO0->FIOCLR = 0X03000000;
+		}
+	}
+}
